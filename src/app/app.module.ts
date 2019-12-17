3873ce8f0d39d735cfe710a8787f48b1f4e2fc1a
Diksha-194 <dikshamankar24@gmail.com>
2019-12-17 14:43:51 +0530

    2 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DescriptionComponent } from './description/description.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, DescriptionComponent, SidemenuComponent, NavComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
